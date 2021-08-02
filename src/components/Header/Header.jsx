8db69f34047d183c8dfdd63d6fba7f3af5598858
Mason <michalecmb@gmail.com>
2021-08-02 14:12:03 +0200

    added logout, login, and registration functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import Button from '../Button/Button';
-import Logo from '../Logo/Logo';
-import './Header.css';
+import React from "react";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import "./Header.css";
 
 export default function Header() {
-	function handleLogout() {
-		localStorage.removeItem('user');
-		window.history.pushState({}, '', '/login');
-	}
-	return (
-		<div className='header'>
-			<Logo />
-			<div className='header-text-wrapper'>
-				<div className='user-info-header'>
-					<p>Mason</p>
-				</div>
-				{/* remove user login token */}
-				<Button text='Logout' onClick={handleLogout} />
-			</div>
-		</div>
-	);
+  function handleLogout() {
+    localStorage.removeItem("user");
+    window.history.replace({}, "", "/login");
+  }
+  return (
+    <div className="header">
+      <Logo />
+      <div className="header-text-wrapper">
+        <div className="user-info-header">
+          <p>Mason</p>
+        </div>
+        {/* remove user login token */}
+        <Button text="Logout" onClick={handleLogout} />
+      </div>
+    </div>
+  );
 }

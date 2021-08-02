d555a58598c2a8cf9cb921112d1ec432430f5269
Mason <michalecmb@gmail.com>
2021-08-02 10:49:57 +0200

    finishing login and registration

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,45 +1,45 @@
-import { React, useState } from "react";
-import { Link } from "react-router-dom";
-import Button from "../Button/Button";
-import "./Login.css";
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
+import './Login.css';
 
 export default function Login() {
-  const [loginState, setLoginState] = useState({
-    username: "",
-    password: "",
-  });
+	const [loginState, setLoginState] = useState({
+		username: '',
+		password: '',
+	});
 
-  function handleUsernameChange(event) {
-    setLoginState({ ...loginState, username: event.target.value });
-  }
+	function handleUsernameChange(event) {
+		setLoginState({ ...loginState, username: event.target.value });
+	}
 
-  function handlePasswordChange(event) {
-    setLoginState({ ...loginState, password: event.target.value });
-  }
+	function handlePasswordChange(event) {
+		setLoginState({ ...loginState, password: event.target.value });
+	}
 
-  return (
-    <div className="login-wrapper">
-      <h2>Login</h2>
-      <form className="login-form">
-        <p className="text-field-header">Username:</p>
-        <input
-          placeholder="Enter username"
-          value={loginState.username}
-          onChange={handleUsernameChange}
-        />
-        <p className="text-field-header">Password:</p>
-        <input
-          type="password"
-          placeholder="Enter password"
-          value={loginState.password}
-          onChange={handlePasswordChange}
-        />
-        <Button className="app-button" text="Login" />
-      </form>
-      <p>
-        If you do not yet have an account, please register{" "}
-        <Link to="/register">here.</Link>
-      </p>
-    </div>
-  );
+	return (
+		<div className='login-wrapper'>
+			<h2>Login</h2>
+			<form className='login-form'>
+				<p className='text-field-header'>Username:</p>
+				<input
+					placeholder='Enter username'
+					value={loginState.username}
+					onChange={handleUsernameChange}
+				/>
+				<p className='text-field-header'>Password:</p>
+				<input
+					type='password'
+					placeholder='Enter password'
+					value={loginState.password}
+					onChange={handlePasswordChange}
+				/>
+				<Button className='app-button' text='Login' />
+			</form>
+			<p>
+				If you do not yet have an account, please register{' '}
+				<Link to='/register'>here.</Link>
+			</p>
+		</div>
+	);
 }

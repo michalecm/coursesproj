1c20e2bf465509b3d7e6f5e06011700f364232ad
Mason <michalecmb@gmail.com>
2021-08-02 14:03:33 +0200

    finishing login and registration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../Button/Button';
-import Logo from '../Logo/Logo';
-import './Header.css';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import "./Header.css";
 
-export default function Header() {
-	return (
-		<div className='header'>
-			<Logo />
-			<div className='header-text-wrapper'>
-				<div className='user-info-header'>
-					<p>Mason</p>
-				</div>
-				<Link to='/login'>
-					{/* remove user login token */}
-					<Button text='Logout' />
-				</Link>
-			</div>
-		</div>
-	);
+export default function Header({ history }) {
+  function handleLogout() {
+    localStorage.removeItem("user");
+    history.push("/login");
+  }
+  return (
+    <div className="header">
+      <Logo />
+      <div className="header-text-wrapper">
+        <div className="user-info-header">
+          <p>Mason</p>
+        </div>
+        {/* remove user login token */}
+        <Button text="Logout" onClick={handleLogout} />
+      </div>
+    </div>
+  );
 }
+
+Header.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+};

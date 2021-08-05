7b91d7c3846a410ebea20da3cb24e7a2f3361fe7
Mason <michalecmb@gmail.com>
2021-08-05 14:49:33 +0200

    added endpoints constants

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import CourseCard from "../CourseCard/CourseCard";
-import Search from "../Search/Search";
-import "./Courses.css";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import CourseCard from '../CourseCard/CourseCard';
+import Search from '../Search/Search';
+import './Courses.css';
 
 export default function Courses() {
-  const allCourses = useSelector((state) => state.coursesReducer.courses);
-  const [searchResults, setSearchResults] = useState(allCourses);
+	const allCourses = useSelector((state) => state.coursesReducer.courses);
+	const [searchResults, setSearchResults] = useState(allCourses);
 
-  function searchFilter(text) {
-    if (!text) {
-      setSearchResults(allCourses);
-    } else {
-      setSearchResults(
-        allCourses.filter(
-          (course) => course.title.includes(text) || course.id.includes(text)
-        )
-      );
-    }
-  }
-  const courses = searchResults.map((course, i) => (
-    <CourseCard
-      key={course.id}
-      id={course.id}
-      title={course.title}
-      description={course.description}
-      creationDate={course.creationDate}
-      authors={course.authors}
-      duration={course.duration}
-    />
-  ));
+	function searchFilter(text) {
+		if (!text) {
+			setSearchResults(allCourses);
+		} else {
+			setSearchResults(
+				allCourses.filter(
+					(course) => course.title.includes(text) || course.id.includes(text)
+				)
+			);
+		}
+	}
+	const courses = searchResults.map((course, i) => (
+		<CourseCard
+			key={course.id}
+			id={course.id}
+			title={course.title}
+			description={course.description}
+			creationDate={course.creationDate}
+			authors={course.authors}
+			duration={course.duration}
+		/>
+	));
 
-  return (
-    <div className="courses-wrapper">
-      <Search cb={searchFilter} />
-      <div className="courses-render-wrapper">{courses}</div>
-    </div>
-  );
+	return (
+		<div className='courses-wrapper'>
+			<Search cb={searchFilter} />
+			<div className='courses-render-wrapper'>{courses}</div>
+		</div>
+	);
 }

59d9d92ccbb6c6c2206133e55e174ed69ee49a33
Mason <michalecmb@gmail.com>
2021-08-06 13:55:09 +0200

    trying to get isLoading to work so that Courses will not load until they are fetched by axios

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,84 +1,83 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Button from '../Button/Button';
-import { mockedAuthorsList } from '../../util/consts';
-import formatDuration from '../../util/funcs';
-import './CourseCard.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Button from "../Button/Button";
+import formatDuration from "../../util/funcs";
+import "./CourseCard.css";
 
 export default function CourseCard({
-	id,
-	title,
-	description,
-	creationDate,
-	duration,
-	authors,
-	userCreatedAuthors,
+  id,
+  title,
+  description,
+  creationDate,
+  duration,
+  authors,
+  userCreatedAuthors,
 }) {
-	const appstate = useSelector((state) => state);
+  const appstate = useSelector((state) => state);
 
-	function getCourseAuthors() {
-		return [...appstate.authorsReducer.authors]
-			.filter((author) => authors.includes(author.id))
-			.map((authorObj) => authorObj.name)
-			.join(', ');
-	}
+  function getCourseAuthors() {
+    return [...appstate.authorsReducer.authors]
+      .filter((author) => authors.includes(author.id))
+      .map((authorObj) => authorObj.name)
+      .join(", ");
+  }
 
-	return (
-		<div id={id} className='course-card-wrapper'>
-			<div className='course-info'>
-				<div className='course-header'>
-					<h2>{title}</h2>
-				</div>
-				<div className='course-about'>{description}</div>
-			</div>
-			<div className='course-meta-info'>
-				<div className='course-metadata'>
-					<div className='subject-left-info-right'>
-						<div className='left-right-header'>Authors:</div>
-						<div className='authors'>{getCourseAuthors()}</div>
-					</div>
-					<div className='subject-left-info-right'>
-						<div className='left-right-header'>Duration:</div>
-						<div>{formatDuration(duration)}</div>
-					</div>
-					<div className='subject-left-info-right'>
-						<div className='left-right-header'>Created:</div>
-						<div>{creationDate}</div>
-					</div>
-				</div>
-				<Link
-					to={{
-						pathname: `/courses/${id}`,
-						state: {
-							id,
-							title,
-							description,
-							creationDate,
-							duration,
-							authors,
-							userCreatedAuthors,
-						},
-					}}
-				>
-					<Button text='Show course' />
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div id={id} className="course-card-wrapper">
+      <div className="course-info">
+        <div className="course-header">
+          <h2>{title}</h2>
+        </div>
+        <div className="course-about">{description}</div>
+      </div>
+      <div className="course-meta-info">
+        <div className="course-metadata">
+          <div className="subject-left-info-right">
+            <div className="left-right-header">Authors:</div>
+            <div className="authors">{getCourseAuthors()}</div>
+          </div>
+          <div className="subject-left-info-right">
+            <div className="left-right-header">Duration:</div>
+            <div>{formatDuration(duration)}</div>
+          </div>
+          <div className="subject-left-info-right">
+            <div className="left-right-header">Created:</div>
+            <div>{creationDate}</div>
+          </div>
+        </div>
+        <Link
+          to={{
+            pathname: `/courses/${id}`,
+            state: {
+              id,
+              title,
+              description,
+              creationDate,
+              duration,
+              authors,
+              userCreatedAuthors,
+            },
+          }}
+        >
+          <Button text="Show course" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 CourseCard.propTypes = {
-	id: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	description: PropTypes.string.isRequired,
-	creationDate: PropTypes.string.isRequired,
-	duration: PropTypes.number.isRequired,
-	authors: PropTypes.arrayOf(PropTypes.string).isRequired,
-	userCreatedAuthors: PropTypes.arrayOf(PropTypes.object),
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  creationDate: PropTypes.string.isRequired,
+  duration: PropTypes.number.isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  userCreatedAuthors: PropTypes.arrayOf(PropTypes.object),
 };
 
 CourseCard.defaultProps = {
-	userCreatedAuthors: [],
+  userCreatedAuthors: [],
 };

e890a4a81a579f08cb4f42bc2d11efdb34fabf5d
Mason <michalecmb@gmail.com>
2021-08-06 13:39:50 +0200

    added endpoints constants

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,52 +1,48 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import CourseCard from "../CourseCard/CourseCard";
-import Search from "../Search/Search";
-import "./Courses.css";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import CourseCard from '../CourseCard/CourseCard';
+import Search from '../Search/Search';
+import './Courses.css';
 
 export default function Courses() {
-  const allCourses = useSelector((state) => state.coursesReducer.courses);
-  const [searchResults, setSearchResults] = useState(allCourses);
+	const allCourses = useSelector((state) => state.coursesReducer.courses);
+	const [searchResults, setSearchResults] = useState(allCourses.courses);
 
-  // eslint-disable-next-line no-console
-  console.log(allCourses);
+	function searchFilter(text) {
+		if (!text) {
+			setSearchResults(allCourses.courses);
+		} else {
+			setSearchResults(
+				allCourses.courses.filter(
+					(course) => course.title.includes(text) || course.id.includes(text)
+				)
+			);
+		}
+	}
+	// eslint-disable-next-line no-console
+	console.log(allCourses.courses);
+	// eslint-disable-next-line no-debugger
+	debugger;
+	const courses = !allCourses.isLoading
+		? searchResults.map((course, i) => (
+				<CourseCard
+					key={course.id}
+					id={course.id}
+					title={course.title}
+					description={course.description}
+					creationDate={course.creationDate}
+					authors={course.authors}
+					duration={course.duration}
+				/>
+		  ))
+		: [];
 
-  function searchFilter(text) {
-    if (!text) {
-      setSearchResults(allCourses);
-    } else {
-      setSearchResults(
-        allCourses.filter(
-          (course) => course.title.includes(text) || course.id.includes(text)
-        )
-      );
-    }
-  }
-  // eslint-disable-next-line no-console
-  console.log(allCourses);
-  // eslint-disable-next-line no-console
-  console.log(typeof allCourses);
-  // eslint-disable-next-line no-debugger
-  debugger;
-  const courses =
-    allCourses.length > 0
-      ? searchResults.map((course, i) => (
-          <CourseCard
-            key={course.id}
-            id={course.id}
-            title={course.title}
-            description={course.description}
-            creationDate={course.creationDate}
-            authors={course.authors}
-            duration={course.duration}
-          />
-        ))
-      : [];
-
-  return (
-    <div className="courses-wrapper">
-      <Search cb={searchFilter} />
-      <div className="courses-render-wrapper">{courses}</div>
-    </div>
-  );
+	return !allCourses.isLoading ? (
+		<div className='courses-wrapper'>
+			<Search cb={searchFilter} />
+			<div className='courses-render-wrapper'>{courses}</div>
+		</div>
+	) : (
+		<div />
+	);
 }
--- src/store/courses/reducer.js
+++ src/store/courses/reducer.js
@@ -1,35 +1,36 @@
-import { ADD_COURSE, DELETE_COURSE, UPDATE_COURSE } from './actionTypes';
+import { ADD_COURSE, DELETE_COURSE, UPDATE_COURSE } from "./actionTypes";
 
 const coursesInitialState = {
-	courses: [], /// default value - empty array. After success getting courses from API - array of courses.
-	// See Swagger `/courses/all`
+  courses: [], /// default value - empty array. After success getting courses from API - array of courses.
+  // See Swagger `/courses/all`
+  isLoading: !coursesInitialState.courses.length > 0,
 };
 
 const coursesReducer = (state = coursesInitialState, action) => {
-	if (action === undefined) {
-		return state;
-	}
+  if (action === undefined) {
+    return state;
+  }
 
-	const { type, payload } = action;
+  const { type, payload } = action;
 
-	switch (type) {
-		case ADD_COURSE:
-			return {
-				courses: [...state.courses, ...payload],
-			};
-		case DELETE_COURSE:
-			return {
-				courses: [...state.courses].filter((course) => course.id !== payload),
-			};
-		case UPDATE_COURSE:
-			return {
-				courses: [...state.courses]
-					.filter((course) => course.id !== payload.id)
-					.push(payload),
-			};
-		default:
-			return state;
-	}
+  switch (type) {
+    case ADD_COURSE:
+      return {
+        courses: [...state.courses, ...payload],
+      };
+    case DELETE_COURSE:
+      return {
+        courses: [...state.courses].filter((course) => course.id !== payload),
+      };
+    case UPDATE_COURSE:
+      return {
+        courses: [...state.courses]
+          .filter((course) => course.id !== payload.id)
+          .push(payload),
+      };
+    default:
+      return state;
+  }
 };
 
 export default coursesReducer;

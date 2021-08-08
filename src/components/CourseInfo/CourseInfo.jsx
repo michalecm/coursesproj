0952d2df0afe92c6fb09a373e9669a1a7ec1ac8d
Mason <michalecmb@gmail.com>
2021-08-08 17:38:58 +0200

    updating courseCard to use redux store, and tmoved getCourseAuthors to util

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,60 +1,56 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import formatDuration, { retrieveAuthorNames } from '../../util/funcs';
-import './CourseInfo.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import formatDuration, { retrieveAuthorNames } from "../../util/funcs";
+import "./CourseInfo.css";
 
 export default function CourseInfo() {
-	const location = useLocation();
-	const myData = location.state;
-	// eslint-disable-next-line no-console
-	console.dir(location.state);
-	// eslint-disable-next-line no-console
-	console.log(myData);
-	const authorsList = useSelector((state) => state.authorsReducer);
-	const coursesList = useSelector((state) => state.coursesReducer);
+  const location = useLocation();
+  const { myData } = location.state;
+  const authorsList = useSelector((state) => state.authorsReducer);
+  const coursesList = useSelector((state) => state.coursesReducer);
 
-	return !authorsList.isLoading && !coursesList.isLoading ? (
-		<div className='course-info-wrapper'>
-			<div className='backlink'>
-				<Link to='/courses'> &#60; Back to courses </Link>
-			</div>
-			<div className='course-info-title'>
-				<h2>{myData.title}</h2>
-			</div>
-			<div className='course-info-bottom'>
-				<div className='course-info-description'>
-					<p>{myData.description}</p>
-				</div>
-				<div>
-					<div className='stateItem'>
-						<div className='stateLabel'>ID: </div>
-						<div className='id'>{myData.id}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Duration: </div>
-						<div className='id'>{formatDuration(myData.duration)}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Created: </div>
-						<div className='id'>{myData.creationDate}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Authors: </div>
-						<div className='id-authors-divs'>
-							{retrieveAuthorNames(myData.authors, authorsList.authors)
-								.sort()
-								.map((author) => (
-									<div key={`${author}key`} className='course-info-author'>
-										<div className='author-space'>{author}</div>
-									</div>
-								))}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	) : (
-		<div />
-	);
+  return !authorsList.isLoading && !coursesList.isLoading ? (
+    <div className="course-info-wrapper">
+      <div className="backlink">
+        <Link to="/courses"> &#60; Back to courses </Link>
+      </div>
+      <div className="course-info-title">
+        <h2>{myData.title}</h2>
+      </div>
+      <div className="course-info-bottom">
+        <div className="course-info-description">
+          <p>{myData.description}</p>
+        </div>
+        <div>
+          <div className="stateItem">
+            <div className="stateLabel">ID: </div>
+            <div className="id">{myData.id}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Duration: </div>
+            <div className="id">{formatDuration(myData.duration)}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Created: </div>
+            <div className="id">{myData.creationDate}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Authors: </div>
+            <div className="id-authors-divs">
+              {retrieveAuthorNames(myData.authors, authorsList.authors)
+                .sort()
+                .map((author) => (
+                  <div key={`${author}key`} className="course-info-author">
+                    <div className="author-space">{author}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div />
+  );
 }
--- src/util/funcs.js
+++ src/util/funcs.js
@@ -1,21 +1,21 @@
 export default function formatDuration(courseTime) {
-  const mins = courseTime % 60;
-  const hrs = (courseTime - mins) / 60;
-  if (hrs < 10) {
-    return `0${hrs}:${mins} hours`;
-  }
-  return `${hrs}:${mins} hours`;
+	const mins = courseTime % 60;
+	const hrs = (courseTime - mins) / 60;
+	if (hrs < 10) {
+		return `0${hrs}:${mins} hours`;
+	}
+	return `${hrs}:${mins} hours`;
 }
 
 export function retrieveAuthorNames(ids = [], authors) {
-  return authors
-    .filter((auth) => ids.includes(auth.id))
-    .map((auth) => auth.name);
+	return authors
+		.filter((auth) => ids.includes(auth.id))
+		.map((auth) => auth.name);
 }
 
 export function getCourseAuthors(allAuthors, courseAuthors) {
-  return allAuthors
-    .filter((author) => courseAuthors.includes(author.id))
-    .map((authorObj) => authorObj.name)
-    .join(", ");
+	return allAuthors
+		.filter((author) => courseAuthors.includes(author.id))
+		.map((authorObj) => authorObj.name)
+		.join(', ');
 }

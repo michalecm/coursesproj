d817e174309b7351099f039251574be6bef77684
Mason <michalecmb@gmail.com>
2021-08-09 14:53:38 +0200

    updating createcourse to use redux and API

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logOut } from '../../store/users/actionCreators';
-import Button from '../Button/Button';
-import Logo from '../Logo/Logo';
-import './Header.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../store/users/actionCreators";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import "./Header.css";
 
 export default function Header() {
-	const dispatch = useDispatch();
-	function handleLogout() {
-		dispatch(logOut());
-		localStorage.removeItem('user');
-		window.location.replace('/login');
-	}
-	return (
-		<div className='header'>
-			<Logo />
-			<div className='header-text-wrapper'>
-				<div className='user-info-header'>
-					<p>Mason</p>
-				</div>
-				{/* remove user login token */}
-				<Button text='Logout' onClick={handleLogout} />
-			</div>
-		</div>
-	);
+  const userName = useSelector((state) => state.userReducer.name);
+  const dispatch = useDispatch();
+  function handleLogout() {
+    dispatch(logOut());
+    localStorage.removeItem("user");
+    window.location.replace("/login");
+  }
+  return (
+    <div className="header">
+      <Logo />
+      <div className="header-text-wrapper">
+        <div className="user-info-header">
+          <p>{userName || ""}</p>
+        </div>
+        {/* remove user login token */}
+        <Button text="Logout" onClick={handleLogout} />
+      </div>
+    </div>
+  );
 }

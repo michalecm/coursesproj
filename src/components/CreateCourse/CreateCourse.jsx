97e09e8fe3c48d7b9c24bd8c6312bd8455a816f8
Mason <michalecmb@gmail.com>
2021-08-09 14:22:49 +0200

    updating createcourse to use redux and API

--- src/components/CreateCourse/CreateCourse.jsx
+++ src/components/CreateCourse/CreateCourse.jsx
@@ -1,215 +1,170 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
-import { mockedAuthorsList } from '../../util/consts';
-import Button from '../Button/Button';
-import Input from '../Input/Input';
-import './CreateCourse.css';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+import APIService from "../../util/APIService";
+import "./CreateCourse.css";
+import { ENDPOINTS } from "../../util/consts";
 
-export default function CreateCourse({
-	coursesList,
-	setCoursesList,
-	userCreatedAuthors,
-	setUserCreatedAuthors,
-	history,
-	authorsList,
-}) {
-	const [newCourseData, setNewCourseData] = useState({
-		authors: {
-			authorField: '',
-			customAuthors: [],
-			defaultAuthors: mockedAuthorsList,
-			chosenAuthors: [],
-		},
-		title: '',
-		description: '',
-		duration: 0,
-		id: uuidv4(),
-	});
+export default function CreateCourse({ history }) {
+  const allAuthors = useSelector((state) => state.authorsReducer.authors);
+  const [newCourseData, setNewCourseData] = useState({
+    authorField: { name: "", id: "" },
+    chosenAuthors: [],
+    title: "",
+    description: "",
+    duration: 0,
+    id: uuidv4(),
+  });
 
-	function addAuthorToChosen(newAuthor) {
-		if (newCourseData.authors.chosenAuthors.includes(newAuthor)) {
-			return;
-		}
-		setNewCourseData({
-			...newCourseData,
-			authors: {
-				...newCourseData.authors,
-				chosenAuthors: [...newCourseData.authors.chosenAuthors, newAuthor],
-			},
-		});
-	}
+  function addAuthorToChosen(newAuthor) {
+    if (newCourseData.chosenAuthors.includes(newAuthor)) {
+      return;
+    }
+    setNewCourseData({
+      ...newCourseData,
+      chosenAuthors: [...newCourseData.chosenAuthors, newAuthor],
+    });
+  }
 
-	function removeAuthorFromChosen(author) {
-		setNewCourseData({
-			...newCourseData,
-			authors: {
-				...newCourseData.authors,
-				authorField: '',
-				chosenAuthors: [
-					...newCourseData.authors.chosenAuthors.filter(
-						(aut) => aut !== author
-					),
-				],
-			},
-		});
-	}
+  function removeAuthorFromChosen(author) {
+    setNewCourseData({
+      ...newCourseData,
+      chosenAuthors: [
+        ...newCourseData.chosenAuthors.filter((aut) => aut !== author),
+      ],
+    });
+  }
 
-	function addCourse(course, userCreatedAuthorsParam) {
-		setUserCreatedAuthors([...userCreatedAuthors, ...userCreatedAuthorsParam]);
-		setCoursesList([course, ...coursesList]);
-	}
+  function handleTitleChange(event) {
+    setNewCourseData({ ...newCourseData, title: event.target.value });
+  }
 
-	function processAddCourse(event) {
-		event.preventDefault();
-		addCourse(
-			{
-				id: newCourseData.id,
-				title: newCourseData.title,
-				description: newCourseData.description,
-				creationDate: new Date().toLocaleDateString(),
-				duration: Number(newCourseData.duration),
-				authors: newCourseData.authors.chosenAuthors.map((author) => author.id),
-			},
-			newCourseData.authors.chosenAuthors
-		);
-		history.push('/courses');
-	}
+  function handleCustomAuthorChange(event) {
+    setNewCourseData({
+      ...newCourseData,
+      authorField: { name: event.target.value, id: "" },
+    });
+  }
 
-	function handleTitleChange(event) {
-		setNewCourseData({ ...newCourseData, title: event.target.value });
-	}
+  function handleDescriptionChange(event) {
+    setNewCourseData({ ...newCourseData, description: event.target.value });
+  }
 
-	function handleCustomAuthorChange(event) {
-		setNewCourseData({
-			...newCourseData,
-			authors: {
-				...newCourseData.authors,
-				authorField: { name: event.target.value, id: '' },
-			},
-		});
-	}
+  function handleDurationChange(event) {
+    setNewCourseData({ ...newCourseData, duration: event.target.value });
+  }
 
-	function addCustomAuthorToCustomAuthors() {
-		setNewCourseData({
-			...newCourseData,
-			authors: {
-				...newCourseData.authors,
-				customAuthors: [
-					...newCourseData.authors.customAuthors,
-					{ ...newCourseData.authors.authorField, id: uuidv4() },
-				],
-			},
-		});
-	}
+  function handleCreateAuthor(author) {
+    // dispatch
+    // add id
+  }
 
-	function handleDescriptionChange(event) {
-		setNewCourseData({ ...newCourseData, description: event.target.value });
-	}
+  function handleCreateCourse(event) {
+    event.preventDefault();
+    APIService.Post(ENDPOINTS.POST_ADD_COURSE, {
+      title: newCourseData.title,
+      description: newCourseData.description,
+      creationDate: new Date().toLocaleDateString(),
+      duration: Number(newCourseData.duration),
+      authors: newCourseData.chosenAuthors.map((author) => author.id),
+      id: newCourseData.id,
+    }).then((res) => {
+      // eslint-disable-next-line no-console
+      console.log(res);
+      history.push("/courses");
+    });
+  }
 
-	function handleDurationChange(event) {
-		setNewCourseData({ ...newCourseData, duration: event.target.value });
-	}
+  const authorsDivs = allAuthors.sort().map((author) => (
+    <div key={`${author.name}key`} className="author-w-button">
+      <div className="author-space">{author.name}</div>
+      <Button text="Add Author" onClick={() => addAuthorToChosen(author)} />
+    </div>
+  ));
 
-	const authorsDivs = [
-		...newCourseData.authors.customAuthors,
-		...newCourseData.authors.defaultAuthors,
-	]
-		.sort()
-		.map((author) => (
-			<div key={`${author.name}key`} className='author-w-button'>
-				<div className='author-space'>{author.name}</div>
-				<Button text='Add Author' onClick={() => addAuthorToChosen(author)} />
-			</div>
-		));
+  const courseAuthorsDivs = newCourseData.chosenAuthors.map((author) => (
+    <div className="author-w-button">
+      <div className="author-space">{author.name}</div>
+      <Button
+        text="Delete Author"
+        onClick={() => removeAuthorFromChosen(author)}
+      />
+    </div>
+  ));
 
-	const courseAuthorsDivs = newCourseData.authors.chosenAuthors.map(
-		(author) => (
-			<div className='author-w-button'>
-				<div className='author-space'>{author.name}</div>
-				<Button
-					text='Delete Author'
-					onClick={() => removeAuthorFromChosen(author)}
-				/>
-			</div>
-		)
-	);
-
-	return (
-		<form className='create-course-wrapper' onSubmit={processAddCourse}>
-			<div className='create-course-top'>
-				<div className='create-course-title-entry-wrapper'>
-					<div className='create-course-title-form'>
-						<p className='text-field-header'>Title</p>
-						<Input
-							placeholder='test'
-							type='text'
-							value={newCourseData.title}
-							onChange={handleTitleChange}
-						/>
-					</div>
-					<div className='create-course-title-button'>
-						<Button className='app-button' text='Create Course' type='submit' />
-					</div>
-				</div>
-				<div className='create-course-description-wrapper'>
-					<p className='text-field-header'>Description</p>
-					<textarea
-						placeholder='test description text'
-						value={newCourseData.description}
-						onChange={handleDescriptionChange}
-					/>
-				</div>
-			</div>
-			<div className='create-course-bottom'>
-				<div className='cc-bottom-left'>
-					<div className='cc-add-author'>
-						<div className='create-author-wrapper'>
-							<h3 className='text-field-header'>Author Name</h3>
-							<Input
-								type='text'
-								placeholder='Enter author name...'
-								onChange={handleCustomAuthorChange}
-								value={newCourseData.authors.authorField.name}
-							/>
-						</div>
-						<Button
-							className='app-button'
-							text='Create Author'
-							onClick={addCustomAuthorToCustomAuthors}
-						/>
-					</div>
-					<div className='cc-add-duration'>
-						<div className='create-duration-wrapper'>
-							<h3 className='text-field-header'>Duration</h3>
-							<Input
-								type='text'
-								value={`${newCourseData.duration}`}
-								onChange={handleDurationChange}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className='cc-bottom-right'>
-					<div className='cc-authors'>
-						<h3>Authors</h3>
-						<div>{authorsDivs}</div>
-					</div>
-					<div className='cc-course-authors'>
-						<h3>Course Authors</h3>
-						<div>{courseAuthorsDivs}</div>
-					</div>
-				</div>
-			</div>
-		</form>
-	);
+  return (
+    <form className="create-course-wrapper" onSubmit={handleCreateCourse}>
+      <div className="create-course-top">
+        <div className="create-course-title-entry-wrapper">
+          <div className="create-course-title-form">
+            <p className="text-field-header">Title</p>
+            <Input
+              placeholder="test"
+              type="text"
+              value={newCourseData.title}
+              onChange={handleTitleChange}
+            />
+          </div>
+          <div className="create-course-title-button">
+            <Button className="app-button" text="Create Course" type="submit" />
+          </div>
+        </div>
+        <div className="create-course-description-wrapper">
+          <p className="text-field-header">Description</p>
+          <textarea
+            placeholder="test description text"
+            value={newCourseData.description}
+            onChange={handleDescriptionChange}
+          />
+        </div>
+      </div>
+      <div className="create-course-bottom">
+        <div className="cc-bottom-left">
+          <div className="cc-add-author">
+            <div className="create-author-wrapper">
+              <h3 className="text-field-header">Author Name</h3>
+              <Input
+                type="text"
+                placeholder="Enter author name..."
+                onChange={handleCustomAuthorChange}
+                value={newCourseData.authorField.name}
+              />
+            </div>
+            <Button
+              className="app-button"
+              text="Create Author"
+              onClick={handleCreateAuthor}
+            />
+          </div>
+          <div className="cc-add-duration">
+            <div className="create-duration-wrapper">
+              <h3 className="text-field-header">Duration</h3>
+              <Input
+                type="text"
+                value={`${newCourseData.duration}`}
+                onChange={handleDurationChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="cc-bottom-right">
+          <div className="cc-authors">
+            <h3>Authors</h3>
+            <div>{authorsDivs}</div>
+          </div>
+          <div className="cc-course-authors">
+            <h3>Course Authors</h3>
+            <div>{courseAuthorsDivs}</div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 CreateCourse.propTypes = {
-	setCoursesList: PropTypes.func.isRequired,
-	coursesList: PropTypes.arrayOf(PropTypes.object).isRequired,
-	userCreatedAuthors: PropTypes.arrayOf(PropTypes.object).isRequired,
-	setUserCreatedAuthors: PropTypes.func.isRequired,
-	history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
-	authorsList: PropTypes.arrayOf(PropTypes.object).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };

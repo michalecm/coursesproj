3022dd2196f2b727e20b772b08f6d972f20b1b73
Mason <michalecmb@gmail.com>
2021-08-09 15:38:33 +0200

    updating createcourse to use redux and API

--- src/store/courses/reducer.js
+++ src/store/courses/reducer.js
@@ -1,47 +1,47 @@
 import {
-  ADD_COURSE,
-  ADD_COURSES,
-  DELETE_COURSE,
-  UPDATE_COURSE,
-} from "./actionTypes";
+	ADD_COURSE,
+	ADD_COURSES,
+	DELETE_COURSE,
+	UPDATE_COURSE,
+} from './actionTypes';
 
 const coursesInitialState = {
-  courses: [], /// default value - empty array. After success getting courses from API - array of courses.
-  // See Swagger `/courses/all`
-  isLoading: true,
+	courses: [], /// default value - empty array. After success getting courses from API - array of courses.
+	// See Swagger `/courses/all`
+	isLoading: true,
 };
 
 const coursesReducer = (state = coursesInitialState, action) => {
-  if (action === undefined) {
-    return state;
-  }
+	if (action === undefined) {
+		return state;
+	}
 
-  const { type, payload } = action;
+	const { type, payload } = action;
 
-  switch (type) {
-    case ADD_COURSE:
-      return {
-        courses: [...state.courses, payload],
-        isLoading: false,
-      };
-    case ADD_COURSES:
-      return {
-        courses: [...state.courses, ...payload],
-        isLoading: false,
-      };
-    case DELETE_COURSE:
-      return {
-        courses: [...state.courses].filter((course) => course.id !== payload),
-      };
-    case UPDATE_COURSE:
-      return {
-        courses: [...state.courses]
-          .filter((course) => course.id !== payload.id)
-          .push(payload),
-      };
-    default:
-      return state;
-  }
+	switch (type) {
+		case ADD_COURSE:
+			return {
+				courses: [...state.courses, payload],
+				isLoading: false,
+			};
+		case ADD_COURSES:
+			return {
+				courses: [...state.courses, ...payload],
+				isLoading: false,
+			};
+		case DELETE_COURSE:
+			return {
+				courses: [...state.courses].filter((course) => course.id !== payload),
+			};
+		case UPDATE_COURSE:
+			return {
+				courses: [...state.courses]
+					.filter((course) => course.id !== payload.id)
+					.push(payload),
+			};
+		default:
+			return state;
+	}
 };
 
 export default coursesReducer;

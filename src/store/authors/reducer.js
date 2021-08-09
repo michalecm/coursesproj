a42d1ed51b5c1d8b0a4321c74dc576b61d4dad9d
Mason <michalecmb@gmail.com>
2021-08-09 16:51:56 +0200

    updating createcourse to use redux and API

--- src/store/authors/reducer.js
+++ src/store/authors/reducer.js
@@ -1,36 +1,36 @@
-import { ADD_AUTHOR, ADD_AUTHORS, DELETE_AUTHOR } from "./actionTypes";
+import { ADD_AUTHOR, ADD_AUTHORS, DELETE_AUTHOR } from './actionTypes';
 
 const authorsInitialState = {
-  authors: [], /// default value - empty array. After success getting courses from API - array of courses.
-  // See Swagger `/courses/all`.
-  isLoading: true,
+	authors: [], /// default value - empty array. After success getting courses from API - array of courses.
+	// See Swagger `/courses/all`.
+	isLoading: true,
 };
 
 const authorsReducer = (state = authorsInitialState, action) => {
-  if (action === undefined) {
-    return state;
-  }
+	if (action === undefined) {
+		return state;
+	}
 
-  const { type, payload } = action;
+	const { type, payload } = action;
 
-  switch (type) {
-    case ADD_AUTHOR:
-      return {
-        authors: [...state.authors, payload],
-        isLoading: false,
-      };
-    case ADD_AUTHORS:
-      return {
-        authors: [...state.authors, ...payload],
-        isLoading: false,
-      };
-    case DELETE_AUTHOR:
-      return {
-        authors: [...state.authors].filter((author) => author.id !== payload),
-      };
-    default:
-      return state;
-  }
+	switch (type) {
+		case ADD_AUTHOR:
+			return {
+				authors: [...state.authors, payload],
+				isLoading: false,
+			};
+		case ADD_AUTHORS:
+			return {
+				authors: [...state.authors, ...payload],
+				isLoading: false,
+			};
+		case DELETE_AUTHOR:
+			return {
+				authors: [...state.authors].filter((author) => author.id !== payload),
+			};
+		default:
+			return state;
+	}
 };
 
 export default authorsReducer;

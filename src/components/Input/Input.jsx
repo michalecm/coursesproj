b085ef92482adae56e1f3c0553e3b10ebc6290dc
Mason <michalecmb@gmail.com>
2021-08-10 17:37:41 +0200

    fixing createcourse buttons and functionality and preventing errors

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Input.css";
 
 export default function Input({ type, placeholder, value, name, onChange }) {
-	return (
-		<input
-			type={type}
-			placeholder={placeholder}
-			value={value}
-			name={name}
-			onChange={onChange}
-		/>
-	);
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      name={name}
+      onChange={onChange}
+    />
+  );
 }
 Input.propTypes = {
-	type: PropTypes.string.isRequired,
-	placeholder: PropTypes.string,
-	value: PropTypes.string,
-	name: PropTypes.string,
-	onChange: PropTypes.func,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 Input.defaultProps = {
-	placeholder: '',
-	value: '',
-	name: '',
-	onChange: () => {},
+  placeholder: "",
+  value: "",
+  name: "",
+  onChange: () => {},
 };

b185ee13e6fcde80a056f97f19531c46d98e70a6
Mason <michalecmb@gmail.com>
2021-08-10 18:14:18 +0200

    fixing registration to use endpoints, also fixing errors

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,87 +1,92 @@
-import { React, useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import { BACKEND_URL } from '../../util/consts';
-import Button from '../Button/Button';
-import './Registration.css';
+import { React, useState } from "react";
+import PropTypes from "prop-types";
+import { ENDPOINTS } from "../../util/consts";
+import { validateEmail } from "../../util/funcs";
+import Button from "../Button/Button";
+import "./Registration.css";
+import APIService from "../../util/APIService";
 
 export default function Registration({ history }) {
-	const [registrationState, setRegistrationState] = useState({
-		name: '',
-		email: '',
-		password: '',
-	});
+  const [registrationState, setRegistrationState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-	function handleEmailChange(event) {
-		setRegistrationState({
-			...registrationState,
-			email: event.target.value,
-		});
-	}
+  function handleEmailChange(event) {
+    setRegistrationState({
+      ...registrationState,
+      email: event.target.value,
+    });
+  }
 
-	function handlePasswordChange(event) {
-		setRegistrationState({
-			...registrationState,
-			password: event.target.value,
-		});
-	}
+  function handlePasswordChange(event) {
+    setRegistrationState({
+      ...registrationState,
+      password: event.target.value,
+    });
+  }
 
-	function handleNameChange(event) {
-		setRegistrationState({ ...registrationState, name: event.target.value });
-	}
+  function handleNameChange(event) {
+    setRegistrationState({ ...registrationState, name: event.target.value });
+  }
 
-	function processFormSubmit(event) {
-		event.preventDefault();
-		axios({
-			method: 'post',
-			url: BACKEND_URL.concat('register'),
-			data: JSON.stringify({
-				name: registrationState.name,
-				email: registrationState.email,
-				password: registrationState.password,
-			}),
-			headers: { 'Content-Type': 'application/json' },
-		})
-			.then((res) => {
-				// eslint-disable-next-line no-console
-				console.log(res);
-				history.push('/login');
-			})
-			.catch((err) => {
-				// eslint-disable-next-line no-console
-				console.log(err);
-			});
-	}
+  function processFormSubmit(event) {
+    if (
+      !(
+        validateEmail(registrationState.email) &&
+        registrationState.name.length > 0 &&
+        registrationState.password.length > 0
+      )
+    ) {
+      // eslint-disable-next-line no-alert
+      alert("Please enter a valid email address, name, and password.");
+      return;
+    }
+    event.preventDefault();
+    APIService.Post(ENDPOINTS.POST_REGISTER, {
+      name: registrationState.name,
+      email: registrationState.email,
+      password: registrationState.password,
+    })
+      .then(() => {
+        history.push("/courses");
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-alert
+        alert(`Registration failed: ${err}`);
+      });
+  }
 
-	return (
-		<div className='registration-wrapper'>
-			<h2>Register</h2>
-			<form className='registration-form' onSubmit={processFormSubmit}>
-				<p className='text-field-header'>Name:</p>
-				<input
-					placeholder='Enter name'
-					value={registrationState.name}
-					onChange={handleNameChange}
-				/>
-				<p className='text-field-header'>Email:</p>
-				<input
-					placeholder='Enter email'
-					value={registrationState.email}
-					onChange={handleEmailChange}
-				/>
-				<p className='text-field-header'>Password:</p>
-				<input
-					type='password'
-					placeholder='Enter password'
-					value={registrationState.password}
-					onChange={handlePasswordChange}
-				/>
-				<Button className='app-button' text='Register' type='submit' />
-			</form>
-		</div>
-	);
+  return (
+    <div className="registration-wrapper">
+      <h2>Register</h2>
+      <form className="registration-form" onSubmit={processFormSubmit}>
+        <p className="text-field-header">Name:</p>
+        <input
+          placeholder="Enter name"
+          value={registrationState.name}
+          onChange={handleNameChange}
+        />
+        <p className="text-field-header">Email:</p>
+        <input
+          placeholder="Enter email"
+          value={registrationState.email}
+          onChange={handleEmailChange}
+        />
+        <p className="text-field-header">Password:</p>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={registrationState.password}
+          onChange={handlePasswordChange}
+        />
+        <Button className="app-button" text="Register" type="submit" />
+      </form>
+    </div>
+  );
 }
 
 Registration.propTypes = {
-	history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };

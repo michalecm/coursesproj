7b38dd0fd7e1f1246dff019062eafb9f75f959e6
Mason <michalecmb@gmail.com>
2021-08-11 16:26:22 +0200

    creating login with role

---
+++ src/components/AdminRouter/AdminRouter.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+
+export default function AdminRoute({ component: Component, ...rest }) {
+  const isAdmin = useSelector((state) => state.userReducer.role);
+  return (
+    <Route
+      /* eslint-disable react/jsx-props-no-spreading */
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("user") && isAdmin ? (
+          /* eslint-disable react/jsx-props-no-spreading */
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/courses",
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+AdminRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+};

e0cb2e1ae061d813af7ce5a5aa19e5aca22eee66
Mason <michalecmb@gmail.com>
2021-08-11 16:29:58 +0200

    creating login with role

--- src/components/PrivateRouter/PrivateRouter.jsx
+++ src/components/PrivateRouter/PrivateRouter.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      /* eslint-disable react/jsx-props-no-spreading */
-      {...rest}
-      render={(props) =>
-        localStorage.getItem("user") ? (
-          /* eslint-disable react/jsx-props-no-spreading */
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-            }}
-          />
-        )
-      }
-    />
-  );
+	return (
+		<Route
+			/* eslint-disable react/jsx-props-no-spreading */
+			{...rest}
+			render={(props) =>
+				localStorage.getItem('user') ? (
+					/* eslint-disable react/jsx-props-no-spreading */
+					<Component {...props} />
+				) : (
+					<Redirect
+						to={{
+							pathname: '/login',
+						}}
+					/>
+				)
+			}
+		/>
+	);
 }
 
 PrivateRoute.propTypes = {
-  component: PropTypes.elementType.isRequired,
+	component: PropTypes.elementType.isRequired,
 };

7c5032c3955e322b4d1a5dc0a79d5256f6f25c0c
Mason <michalecmb@gmail.com>
2021-08-11 16:34:34 +0200

    creating login with role

--- src/components/AdminRouter/AdminRouter.jsx
+++ src/components/AdminRouter/AdminRouter.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
 export default function AdminRoute({ component: Component, ...rest }) {
-	const isAdmin = useSelector((state) => state.userReducer.role);
-	return (
-		<Route
-			/* eslint-disable react/jsx-props-no-spreading */
-			{...rest}
-			render={(props) =>
-				localStorage.getItem('user') && isAdmin === 'admin' ? (
-					/* eslint-disable react/jsx-props-no-spreading */
-					<Component {...props} />
-				) : (
-					<Redirect
-						to={{
-							pathname: '/courses',
-						}}
-					/>
-				)
-			}
-		/>
-	);
+  const isAdmin = useSelector((state) => state.userReducer);
+  // eslint-disable-next-line no-console
+  console.log(isAdmin.role + " " + isAdmin.name);
+  return (
+    <Route
+      /* eslint-disable react/jsx-props-no-spreading */
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("user") && isAdmin.role === "admin" ? (
+          /* eslint-disable react/jsx-props-no-spreading */
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/courses",
+            }}
+          />
+        )
+      }
+    />
+  );
 }
 
 AdminRoute.propTypes = {
-	component: PropTypes.elementType.isRequired,
+  component: PropTypes.elementType.isRequired,
 };

73207c3e9e98232891799ff749b96bb1b009377f
Mason <michalecmb@gmail.com>
2021-08-11 15:37:56 +0200

    creating private route

---
+++ src/components/PrivateRouter/PrivateRouter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Redirect, Route } from "react-router-dom";
+
+import PropTypes from "prop-types";
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      /* eslint-disable react/jsx-props-no-spreading */
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("user") ? (
+          /* eslint-disable react/jsx-props-no-spreading */
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: props.linkto ? props.linkto : "/login",
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+  linkto: PropTypes.string.isRequired,
+};

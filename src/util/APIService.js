308d4e00c15383acca33c13f0512ca203c4dc734
Mason <michalecmb@gmail.com>
2021-08-12 16:13:33 +0200

    fixing API service redundancies

--- src/util/APIService.js
+++ src/util/APIService.js
@@ -4,62 +4,35 @@ import { BACKEND_URL } from "./consts";
 export default class APIService {
   static url = BACKEND_URL;
 
-  static Get(endpoint, token = "") {
+  static Get(endpoint, token = "", queries = {}) {
+    const config = {
+      params: queries,
+      headers: { Authorization: token },
+    };
     return axios
-      .get(`${this.url + endpoint}`, { headers: { Authorization: token } })
-      .then((res) => (res.data ? res.data.result : res.result))
-      .catch((err) => err);
-  }
-
-  static Get(endpoint, queries = {}, token = "") {
-    return axios
-      .get(
-        `${this.url + endpoint}`,
-        queries ? { params: queries } : {},
-        token ? { headers: { Authorization: token } } : {}
-      )
+      .get(`${this.url + endpoint}`, config)
       .then((res) => (res.data ? res.data.result : res.result))
       .catch((err) => err);
   }
 
   static Post(endpoint, rawdata = {}, token = "") {
-    return new Promise((resolve, reject) => {
-      try {
-        axios({
-          method: "post",
-          url: `${this.url + endpoint}`,
-          data: rawdata,
-          headers: { Authorization: token },
-        })
-          .then((res) => {
-            resolve(res.data);
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      } catch {
-        const response = "failed to post";
-        reject(response);
-      }
-    });
+    return axios({
+      method: "post",
+      url: `${this.url + endpoint}`,
+      data: rawdata,
+      headers: { Authorization: token },
+    })
+      .then((res) => res.data)
+      .catch((err) => err);
   }
 
   static DELETE(endpoint, specifier = "", token = "") {
-    return new Promise((resolve, reject) => {
-      try {
-        axios({
-          method: "delete",
-          url: `${`${this.url + endpoint}/${specifier}`}`,
-          headers: { Authorization: token },
-        })
-          .then((res) => resolve(res.data))
-          .catch((err) => {
-            reject(err);
-          });
-      } catch {
-        const response = "failed to delete";
-        reject(response);
-      }
-    });
+    return axios({
+      method: "delete",
+      url: `${`${this.url + endpoint}/${specifier}`}`,
+      headers: { Authorization: token },
+    })
+      .then((res) => res.data)
+      .catch((err) => err);
   }
 }

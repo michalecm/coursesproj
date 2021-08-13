95bd72f7d06df6a174498b5626537ff6b751a83a
Mason <michalecmb@gmail.com>
2021-08-13 13:02:45 +0200

    fixing CourseForm

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,56 +1,62 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import formatDuration, { retrieveAuthorNames } from '../../util/funcs';
-import './CourseInfo.css';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import formatDuration, { retrieveAuthorNames } from "../../util/funcs";
+import "./CourseInfo.css";
 
 export default function CourseInfo() {
-	const location = useLocation();
-	const { myData } = location.state;
-	const authorsList = useSelector((state) => state.authorsReducer);
-	const coursesList = useSelector((state) => state.coursesReducer);
+  const authorsList = useSelector((state) => state.authorsReducer);
+  const coursesList = useSelector((state) => state.coursesReducer);
+  const [courseData, setCourseData] = useState({});
 
-	return !authorsList.isLoading && !coursesList.isLoading ? (
-		<div className='course-info-wrapper'>
-			<div className='backlink'>
-				<Link to='/courses'> &#60; Back to courses </Link>
-			</div>
-			<div className='course-info-title'>
-				<h2>{myData.title}</h2>
-			</div>
-			<div className='course-info-bottom'>
-				<div className='course-info-description'>
-					<p>{myData.description}</p>
-				</div>
-				<div>
-					<div className='stateItem'>
-						<div className='stateLabel'>ID: </div>
-						<div className='id'>{myData.id}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Duration: </div>
-						<div className='id'>{formatDuration(myData.duration)}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Created: </div>
-						<div className='id'>{myData.creationDate}</div>
-					</div>
-					<div className='stateItem'>
-						<div className='stateLabel'>Authors: </div>
-						<div className='id-authors-divs'>
-							{retrieveAuthorNames(myData.authors, authorsList.authors)
-								.sort()
-								.map((author) => (
-									<div key={`${author}key`} className='course-info-author'>
-										<div className='author-space'>{author}</div>
-									</div>
-								))}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	) : (
-		<div />
-	);
+  const { id: slug } = useParams();
+  useEffect(() => {
+    let data = coursesList.courses.filter((course) => course.id === slug);
+    [data] = data;
+    setCourseData(data);
+  }, [coursesList]);
+
+  return !authorsList.isLoading && !coursesList.isLoading ? (
+    <div className="course-info-wrapper">
+      <div className="backlink">
+        <Link to="/courses"> &#60; Back to courses </Link>
+      </div>
+      <div className="course-info-title">
+        <h2>{courseData.title}</h2>
+      </div>
+      <div className="course-info-bottom">
+        <div className="course-info-description">
+          <p>{courseData.description}</p>
+        </div>
+        <div>
+          <div className="stateItem">
+            <div className="stateLabel">ID: </div>
+            <div className="id">{courseData.id}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Duration: </div>
+            <div className="id">{formatDuration(courseData.duration)}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Created: </div>
+            <div className="id">{courseData.creationDate}</div>
+          </div>
+          <div className="stateItem">
+            <div className="stateLabel">Authors: </div>
+            <div className="id-authors-divs">
+              {retrieveAuthorNames(courseData.authors, authorsList.authors)
+                .sort()
+                .map((author) => (
+                  <div key={`${author}key`} className="course-info-author">
+                    <div className="author-space">{author}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div />
+  );
 }
--- src/util/APIService.js
+++ src/util/APIService.js
@@ -1,49 +1,49 @@
-import axios from "axios";
-import { BACKEND_URL } from "./consts";
+import axios from 'axios';
+import { BACKEND_URL } from './consts';
 
 export default class APIService {
-  static url = BACKEND_URL;
+	static url = BACKEND_URL;
 
-  static Get(endpoint, queries = {}, token = "") {
-    const config = {
-      params: queries,
-      headers: { Authorization: token },
-    };
-    return axios
-      .get(`${this.url + endpoint}`, config)
-      .then((res) => (res.data ? res.data.result : res.result))
-      .catch((err) => err);
-  }
+	static Get(endpoint, queries = {}, token = '') {
+		const config = {
+			params: queries,
+			headers: { Authorization: token },
+		};
+		return axios
+			.get(`${this.url + endpoint}`, config)
+			.then((res) => (res.data ? res.data.result : res.result))
+			.catch((err) => err);
+	}
 
-  static Post(endpoint, rawdata = {}, token = "") {
-    return axios({
-      method: "post",
-      url: `${this.url + endpoint}`,
-      data: rawdata,
-      headers: { Authorization: token },
-    })
-      .then((res) => res.data)
-      .catch((err) => err);
-  }
+	static Post(endpoint, rawdata = {}, token = '') {
+		return axios({
+			method: 'post',
+			url: `${this.url + endpoint}`,
+			data: rawdata,
+			headers: { Authorization: token },
+		})
+			.then((res) => res.data)
+			.catch((err) => err);
+	}
 
-  static Put(endpoint, specifier = "", rawdata = {}, token = "") {
-    return axios({
-      method: "put",
-      url: `${`${this.url + endpoint}/${specifier}`}`,
-      data: rawdata,
-      headers: { Authorization: token },
-    })
-      .then((res) => res.data)
-      .catch((err) => err);
-  }
+	static Put(endpoint, specifier = '', rawdata = {}, token = '') {
+		return axios({
+			method: 'put',
+			url: `${`${this.url + endpoint}/${specifier}`}`,
+			data: rawdata,
+			headers: { Authorization: token },
+		})
+			.then((res) => res.data)
+			.catch((err) => err);
+	}
 
-  static DELETE(endpoint, specifier = "", token = "") {
-    return axios({
-      method: "delete",
-      url: `${`${this.url + endpoint}/${specifier}`}`,
-      headers: { Authorization: token },
-    })
-      .then((res) => res.data)
-      .catch((err) => err);
-  }
+	static DELETE(endpoint, specifier = '', token = '') {
+		return axios({
+			method: 'delete',
+			url: `${`${this.url + endpoint}/${specifier}`}`,
+			headers: { Authorization: token },
+		})
+			.then((res) => res.data)
+			.catch((err) => err);
+	}
 }

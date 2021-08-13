e535a0470107fa44f4118b61a33b81f2f71611ce
Mason <michalecmb@gmail.com>
2021-08-13 16:48:12 +0200

    fixing users and course  to thunk dispatches

--- src/components/CourseForm/CourseForm.jsx
+++ src/components/CourseForm/CourseForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import { MdDelete } from "react-icons/md";
@@ -34,8 +34,13 @@ export default function CourseForm({ history }) {
     id: uuidv4(),
   });
 
+  const isFirstRun = useRef(true);
   useEffect(() => {
-    history.push("/courses");
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+    } else {
+      history.push("/courses");
+    }
   }, [history, allCourses]);
 
   useEffect(() => {

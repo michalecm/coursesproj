9edea2aaf7b55520a221348041867b0c2ca9d2d6
Mason <michalecmb@gmail.com>
2021-08-13 15:59:08 +0200

    fixing users and course  to thunk dispatches

--- src/store/courses/thunk.js
+++ src/store/courses/thunk.js
@@ -1,56 +1,56 @@
-import { ENDPOINTS } from "../../util/consts";
+import { ENDPOINTS } from '../../util/consts';
 import {
-  deleteCourse,
-  updateCourse,
-  addCourse,
-  addCourses,
-} from "./actionCreators";
-import APIService from "../../util/APIService";
+	deleteCourse,
+	updateCourse,
+	addCourse,
+	addCourses,
+} from './actionCreators';
+import APIService from '../../util/APIService';
 
 export const postDeleteCourse = (id, token) => (dispatch) => {
-  APIService.DELETE(ENDPOINTS.DELETE_COURSE_BY_ID, id, token)
-    .then((res) => {
-      if (res.successful) dispatch(deleteCourse(id));
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("you are not logged in as admin");
-    });
+	APIService.DELETE(ENDPOINTS.DELETE_COURSE_BY_ID, id, token)
+		.then((res) => {
+			if (res.successful) dispatch(deleteCourse(id));
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('you are not logged in as admin');
+		});
 };
 
 export const postUpdateCourse = (id, courseData, token) => (dispatch) => {
-  // eslint-disable-next-line no-console
-  console.dir(courseData);
-  // eslint-disable-next-line no-console
-  console.dir(id + " " + token);
-  APIService.Put(ENDPOINTS.PUT_COURSE_BY_ID, id, courseData, token)
-    .then((res) => {
-      dispatch(updateCourse(res.result));
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("you are not logged in as admin");
-    });
+	// eslint-disable-next-line no-console
+	console.dir(courseData);
+	// eslint-disable-next-line no-console
+	console.dir(`${id} ${token}`);
+	APIService.Put(ENDPOINTS.PUT_COURSE_BY_ID, id, courseData, token)
+		.then((res) => {
+			dispatch(updateCourse(res.result));
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('you are not logged in as admin');
+		});
 };
 
 export const postAddCourse = (courseData, token) => (dispatch) => {
-  APIService.Post(ENDPOINTS.POST_ADD_COURSE, courseData, token)
-    .then((res) => {
-      dispatch(addCourse(res.result));
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("you are not logged in as admin");
-    });
+	APIService.Post(ENDPOINTS.POST_ADD_COURSE, courseData, token)
+		.then((res) => {
+			dispatch(addCourse(res.result));
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('you are not logged in as admin');
+		});
 };
 
 export const getAllCourses = () => (dispatch) => {
-  APIService.Get(ENDPOINTS.GET_ALL_COURSES)
-    .then((courses) => {
-      dispatch(addCourses(courses));
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("you are not logged in as admin");
-    });
+	APIService.Get(ENDPOINTS.GET_ALL_COURSES)
+		.then((courses) => {
+			dispatch(addCourses(courses));
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('you are not logged in as admin');
+		});
 };
--- src/store/users/thunk.js
+++ src/store/users/thunk.js
@@ -1,53 +1,55 @@
-import APIService from '../../util/APIService';
-import { ENDPOINTS } from '../../util/consts';
-import { logOut, logIn, getRole } from './actionCreators';
+import APIService from "../../util/APIService";
+import { ENDPOINTS } from "../../util/consts";
+import { logOut, logIn, getRole } from "./actionCreators";
 
 export const postLogOut = (token) => (dispatch) => {
-	APIService.Delete(ENDPOINTS.DELETE_LOGOUT, '', token)
-		.then((res) => {
-			dispatch(logOut());
-			localStorage.removeItem('user');
-			window.location.replace('/login');
-		})
-		.catch((err) => {
-			// eslint-disable-next-line no-alert
-			alert('you are not logged in as admin');
-		});
+  APIService.Delete(ENDPOINTS.DELETE_LOGOUT, "", token)
+    .then((res) => {
+      dispatch(logOut());
+      localStorage.removeItem("user");
+      window.location.replace("/login");
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("you are not logged in as admin");
+    });
 };
 
 export const postLogIn = (userData) => (dispatch) => {
-	let data = { ...userData };
-	APIService.Post(ENDPOINTS.POST_LOGIN, userData)
-		.then((res) => {
-			data = {
-				...res,
-				...data,
-				name: res.user.name ? res.user.name : 'admin',
-			};
-			localStorage.setItem('user', res.result);
-			dispatch(logIn(data));
-			dispatch(getUserRole());
-		})
-		.catch((err) => {
-			// eslint-disable-next-line no-alert
-			alert('you are not logged in as admin');
-		});
+  let data = { ...userData };
+  APIService.Post(ENDPOINTS.POST_LOGIN, userData)
+    .then((res) => {
+      data = {
+        ...res,
+        ...data,
+        name: res.user.name ? res.user.name : "admin",
+      };
+      localStorage.setItem("user", res.result);
+      // eslint-disable-next-line no-console
+      console.log(localStorage.get("user"));
+      dispatch(logIn(data));
+      dispatch(getUserRole());
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("you are not logged in as admin");
+    });
 };
 
 export const getUserRole = () => (dispatch) => {
-	APIService.Get(ENDPOINTS.GET_CURRENT_USER, {}, localStorage.getItem('user'))
-		.then((resp) => {
-			dispatch(getRole(resp.role));
-		})
-		.catch((err) => {
-			// eslint-disable-next-line no-alert
-			alert('Login failed: user does not exist or incorrect credentials.');
-		});
+  APIService.Get(ENDPOINTS.GET_CURRENT_USER, {}, localStorage.getItem("user"))
+    .then((resp) => {
+      dispatch(getRole(resp.role));
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("Login failed: user does not exist or incorrect credentials.");
+    });
 };
 
 export const registerUser = (userData) => async () => {
-	APIService.Post(ENDPOINTS.POST_REGISTER, userData).catch((err) => {
-		// eslint-disable-next-line no-alert
-		alert(`Registration failed: ${err}`);
-	});
+  APIService.Post(ENDPOINTS.POST_REGISTER, userData).catch((err) => {
+    // eslint-disable-next-line no-alert
+    alert(`Registration failed: ${err}`);
+  });
 };

fec2327ea7843b6010853ae34e4f8d4cf6b3ddd8
Mason <michalecmb@gmail.com>
2021-08-13 16:32:45 +0200

    fixing users and course  to thunk dispatches

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,72 +1,76 @@
-import { React, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
-import Button from '../Button/Button';
-import { validateEmail } from '../../util/funcs';
-import './Login.css';
-import { postLogIn } from '../../store/users/thunk';
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+import { validateEmail } from "../../util/funcs";
+import "./Login.css";
+import { postLogIn } from "../../store/users/thunk";
 
 export default function Login({ history }) {
-	const [loginState, setLoginState] = useState({
-		email: '',
-		password: '',
-	});
+  const [loginState, setLoginState] = useState({
+    email: "",
+    password: "",
+  });
 
-	const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.userReducer.isAuth);
 
-	function handleUsernameChange(event) {
-		setLoginState({ ...loginState, email: event.target.value });
-	}
+  useEffect(() => {
+    if (isAuth) history.push("/courses");
+  }, [isAuth, history]);
 
-	function handlePasswordChange(event) {
-		setLoginState({ ...loginState, password: event.target.value });
-	}
+  const dispatch = useDispatch();
 
-	function processLogin(event) {
-		event.preventDefault();
-		if (!validateEmail(loginState.email) || !loginState.password.length > 0) {
-			// eslint-disable-next-line no-alert
-			alert('Your password or email is invalid.');
-			return;
-		}
-		dispatch(
-			postLogIn({
-				email: loginState.email,
-				password: loginState.password,
-			})
-		);
+  function handleUsernameChange(event) {
+    setLoginState({ ...loginState, email: event.target.value });
+  }
 
-		history.push('/courses');
-	}
+  function handlePasswordChange(event) {
+    setLoginState({ ...loginState, password: event.target.value });
+  }
 
-	return (
-		<div className='login-wrapper'>
-			<h2>Login</h2>
-			<form className='login-form' onSubmit={processLogin}>
-				<p className='text-field-header'>Username:</p>
-				<input
-					placeholder='Enter username'
-					value={loginState.username}
-					onChange={handleUsernameChange}
-				/>
-				<p className='text-field-header'>Password:</p>
-				<input
-					type='password'
-					placeholder='Enter password'
-					value={loginState.password}
-					onChange={handlePasswordChange}
-				/>
-				<Button className='app-button' text='Login' type='submit' />
-			</form>
-			<p>
-				If you do not yet have an account, please register{' '}
-				<Link to='/register'>here.</Link>
-			</p>
-		</div>
-	);
+  function processLogin(event) {
+    event.preventDefault();
+    if (!validateEmail(loginState.email) || !loginState.password.length > 0) {
+      // eslint-disable-next-line no-alert
+      alert("Your password or email is invalid.");
+      return;
+    }
+    dispatch(
+      postLogIn({
+        email: loginState.email,
+        password: loginState.password,
+      })
+    );
+  }
+
+  return (
+    <div className="login-wrapper">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={processLogin}>
+        <p className="text-field-header">Username:</p>
+        <input
+          placeholder="Enter username"
+          value={loginState.username}
+          onChange={handleUsernameChange}
+        />
+        <p className="text-field-header">Password:</p>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={loginState.password}
+          onChange={handlePasswordChange}
+        />
+        <Button className="app-button" text="Login" type="submit" />
+      </form>
+      <p>
+        If you do not yet have an account, please register{" "}
+        <Link to="/register">here.</Link>
+      </p>
+    </div>
+  );
 }
 
 Login.propTypes = {
-	history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
--- src/store/users/thunk.js
+++ src/store/users/thunk.js
@@ -1,62 +1,62 @@
-import APIService from "../../util/APIService";
-import { ENDPOINTS } from "../../util/consts";
-import { logOut, logIn, getRole } from "./actionCreators";
+import APIService from '../../util/APIService';
+import { ENDPOINTS } from '../../util/consts';
+import { logOut, logIn, getRole } from './actionCreators';
 
 export const postLogOut = (token) => (dispatch) => {
-  APIService.Delete(ENDPOINTS.DELETE_LOGOUT, "", token)
-    .then((res) => {
-      dispatch(logOut());
-      localStorage.removeItem("user");
-      window.location.replace("/login");
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("you are not logged in as admin");
-    });
+	APIService.Delete(ENDPOINTS.DELETE_LOGOUT, '', token)
+		.then((res) => {
+			dispatch(logOut());
+			localStorage.removeItem('user');
+			window.location.replace('/login');
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('you are not logged in as admin');
+		});
 };
 
 export const postLogIn = (userData) => (dispatch) => {
-  let data = {};
-  APIService.Post(ENDPOINTS.POST_LOGIN, userData)
-    .then((res) => {
-      // eslint-disable-next-line no-console
-      console.dir(res);
-      // eslint-disable-next-line no-console
-      console.dir(data);
-      data = {
-        token: res.result,
-        isAuth: res.successful,
-        name: res.user.name ? res.user.name : "admin",
-        email: res.email,
-      };
-      localStorage.setItem("user", res.result);
-      // eslint-disable-next-line no-console
-      console.log(localStorage.getItem("user"));
-      dispatch(logIn(data));
-      dispatch(getUserRole());
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("login failed");
-      // eslint-disable-next-line no-console
-      console.dir(err);
-    });
+	let data = {};
+	APIService.Post(ENDPOINTS.POST_LOGIN, userData)
+		.then((res) => {
+			// eslint-disable-next-line no-console
+			console.dir(res);
+			// eslint-disable-next-line no-console
+			console.dir(data);
+			data = {
+				token: res.result,
+				isAuth: res.successful,
+				name: res.user.name ? res.user.name : 'admin',
+				email: res.email,
+			};
+			localStorage.setItem('user', res.result);
+			// eslint-disable-next-line no-console
+			console.log(localStorage.getItem('user'));
+			dispatch(logIn(data));
+			dispatch(getUserRole());
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('login failed');
+			// eslint-disable-next-line no-console
+			console.dir(err);
+		});
 };
 
 export const getUserRole = () => (dispatch) => {
-  APIService.Get(ENDPOINTS.GET_CURRENT_USER, {}, localStorage.getItem("user"))
-    .then((resp) => {
-      dispatch(getRole(resp.role));
-    })
-    .catch((err) => {
-      // eslint-disable-next-line no-alert
-      alert("Login failed: user does not exist or incorrect credentials.");
-    });
+	APIService.Get(ENDPOINTS.GET_CURRENT_USER, {}, localStorage.getItem('user'))
+		.then((resp) => {
+			dispatch(getRole(resp.role));
+		})
+		.catch((err) => {
+			// eslint-disable-next-line no-alert
+			alert('Login failed: user does not exist or incorrect credentials.');
+		});
 };
 
 export const registerUser = (userData) => async () => {
-  APIService.Post(ENDPOINTS.POST_REGISTER, userData).catch((err) => {
-    // eslint-disable-next-line no-alert
-    alert(`Registration failed: ${err}`);
-  });
+	APIService.Post(ENDPOINTS.POST_REGISTER, userData).catch((err) => {
+		// eslint-disable-next-line no-alert
+		alert(`Registration failed: ${err}`);
+	});
 };

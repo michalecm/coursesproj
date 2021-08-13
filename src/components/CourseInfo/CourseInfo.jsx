ccd97f0bf3f616e7b769923f1b5b14c3949c6938
Mason <michalecmb@gmail.com>
2021-08-13 13:24:19 +0200

    fixing authors to thunk dispatches

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import formatDuration, { retrieveAuthorNames } from "../../util/funcs";
-import "./CourseInfo.css";
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import formatDuration, { retrieveAuthorNames } from '../../util/funcs';
+import './CourseInfo.css';
 
 export default function CourseInfo() {
-  const authorsList = useSelector((state) => state.authorsReducer);
-  const coursesList = useSelector((state) => state.coursesReducer);
-  const [courseData, setCourseData] = useState({});
+	const authorsList = useSelector((state) => state.authorsReducer);
+	const coursesList = useSelector((state) => state.coursesReducer);
+	const [courseData, setCourseData] = useState({});
 
-  const { id: slug } = useParams();
-  useEffect(() => {
-    let data = coursesList.courses.filter((course) => course.id === slug);
-    [data] = data;
-    setCourseData(data);
-  }, [coursesList, slug]);
+	const { id: slug } = useParams();
+	useEffect(() => {
+		let data = coursesList.courses.filter((course) => course.id === slug);
+		[data] = data;
+		setCourseData(data);
+	}, [coursesList, slug]);
 
-  return !authorsList.isLoading && !coursesList.isLoading ? (
-    <div className="course-info-wrapper">
-      <div className="backlink">
-        <Link to="/courses"> &#60; Back to courses </Link>
-      </div>
-      <div className="course-info-title">
-        <h2>{courseData.title}</h2>
-      </div>
-      <div className="course-info-bottom">
-        <div className="course-info-description">
-          <p>{courseData.description}</p>
-        </div>
-        <div>
-          <div className="stateItem">
-            <div className="stateLabel">ID: </div>
-            <div className="id">{courseData.id}</div>
-          </div>
-          <div className="stateItem">
-            <div className="stateLabel">Duration: </div>
-            <div className="id">{formatDuration(courseData.duration)}</div>
-          </div>
-          <div className="stateItem">
-            <div className="stateLabel">Created: </div>
-            <div className="id">{courseData.creationDate}</div>
-          </div>
-          <div className="stateItem">
-            <div className="stateLabel">Authors: </div>
-            <div className="id-authors-divs">
-              {retrieveAuthorNames(courseData.authors, authorsList.authors)
-                .sort()
-                .map((author) => (
-                  <div key={`${author}key`} className="course-info-author">
-                    <div className="author-space">{author}</div>
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div />
-  );
+	return !authorsList.isLoading && !coursesList.isLoading ? (
+		<div className='course-info-wrapper'>
+			<div className='backlink'>
+				<Link to='/courses'> &#60; Back to courses </Link>
+			</div>
+			<div className='course-info-title'>
+				<h2>{courseData.title}</h2>
+			</div>
+			<div className='course-info-bottom'>
+				<div className='course-info-description'>
+					<p>{courseData.description}</p>
+				</div>
+				<div>
+					<div className='stateItem'>
+						<div className='stateLabel'>ID: </div>
+						<div className='id'>{courseData.id}</div>
+					</div>
+					<div className='stateItem'>
+						<div className='stateLabel'>Duration: </div>
+						<div className='id'>{formatDuration(courseData.duration)}</div>
+					</div>
+					<div className='stateItem'>
+						<div className='stateLabel'>Created: </div>
+						<div className='id'>{courseData.creationDate}</div>
+					</div>
+					<div className='stateItem'>
+						<div className='stateLabel'>Authors: </div>
+						<div className='id-authors-divs'>
+							{retrieveAuthorNames(courseData.authors, authorsList.authors)
+								.sort()
+								.map((author) => (
+									<div key={`${author}key`} className='course-info-author'>
+										<div className='author-space'>{author}</div>
+									</div>
+								))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	) : (
+		<div />
+	);
 }
---
+++ src/store/authors/thunk.js
@@ -0,0 +1,30 @@
+import APIService from "../../util/APIService";
+import { ENDPOINTS } from "../../util/consts";
+import { addAuthor, addAuthors, deleteAuthor } from "./actionCreators";
+
+export const getAllAuthors = () => async (dispatch) => {
+  const authors = await APIService.Get(ENDPOINTS.GET_ALL_AUTHORS);
+  dispatch(addAuthors(authors));
+};
+
+export const postAuthor = (authData, token) => async (dispatch) => {
+  APIService.Post(ENDPOINTS.POST_ADD_AUTHOR, authData, token)
+    .then((res) => {
+      dispatch(addAuthor({ name: authData, id: res.result.id }));
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("you are not logged in as admin");
+    });
+};
+
+export const postDeleteAuthor = (authId, token) => async (dispatch) => {
+  APIService.Delete(ENDPOINTS.DELETE_AUTHOR_BY_ID, authId, token)
+    .then((res) => {
+      if (res.successful) dispatch(deleteAuthor(authId));
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("you are not logged in as admin");
+    });
+};

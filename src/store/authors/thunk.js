fcac594bc6b64095a2cac030537234d5e8c106bc
Mason <michalecmb@gmail.com>
2021-08-13 13:27:33 +0200

    fixing authors to thunk dispatches

--- src/store/authors/thunk.js
+++ src/store/authors/thunk.js
@@ -3,8 +3,12 @@ import { ENDPOINTS } from "../../util/consts";
 import { addAuthor, addAuthors, deleteAuthor } from "./actionCreators";
 
 export const getAllAuthors = () => async (dispatch) => {
-  const authors = await APIService.Get(ENDPOINTS.GET_ALL_AUTHORS);
-  dispatch(addAuthors(authors));
+  APIService.Get(ENDPOINTS.GET_ALL_AUTHORS)
+    .then((authors) => dispatch(addAuthors(authors)))
+    .catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert("you are not logged in as admin");
+    });
 };
 
 export const postAuthor = (authData, token) => async (dispatch) => {
